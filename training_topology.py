0819e002272d8c62ff8382b9291ca061c03dca04
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2023-09-12 20:37:20 -0700

    Update

--- training_topology.py
+++ training_topology.py
@@ -4,30 +4,30 @@ hparams = {
     "data": {
         "window_size": 24,
         "stride_length": 1,
-        "lead_time": 1,
-        "normalized": True
+        "lead_time": 3,
+        "normalized": True,
     },
     "model": {
         "architecture": None,  # set below
         "num_layers": 20,
-        "hidden_channels": 64,
+        "hidden_channels": 128,
         "param_sharing": False,
-        "graff_step_size": 1,
         "edge_orientation": None,  # set below
-        "adjacency_type": None  # set below
+        "adjacency_type": None,  # set below
     },
     "training": {
-        "num_epochs": 20,
+        "num_epochs": 30,
         "batch_size": 64,
         "learning_rate": 1e-4,
         "weight_decay": 0,
         "random_seed": 42,
         "train_years": None,  # set below
-        "holdout_size": 0.15,
+        "holdout_size": 3/18 * 15/18,
     }
 }
 
 DATASET_PATH = "/scratch/kirschstein/LamaH-CE"
+CHECKPOINT_PATH = "./runs/topology"
 
 for fold, (train_years, test_years) in enumerate(functions.k_fold_cross_validation_split(range(2000, 2018), k=6)):
     for architecture in ["GCN", "ResGCN", "GCNII"]:
@@ -40,10 +40,10 @@ for fold, (train_years, test_years) in enumerate(functions.k_fold_cross_validati
 
                 functions.ensure_reproducibility(hparams["training"]["random_seed"])
 
-                dataset = functions.load_dataset(DATASET_PATH, hparams, "train")
+                dataset = functions.load_dataset(DATASET_PATH, hparams, split="train")
                 model = functions.construct_model(hparams, dataset)
                 history = functions.train(model, dataset, hparams)
 
                 functions.save_checkpoint(history, hparams,
                                           f"{architecture}_{edge_orientation}_{adjacency_type}_{fold}.run",
-                                          directory="./runs/topology")
+                                          directory=CHECKPOINT_PATH)
