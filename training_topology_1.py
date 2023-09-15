8003bc1488966053915642a83c4d88a806201bb8
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2023-09-14 21:44:45 -0700

    Update

--- training_topology_1.py
+++ training_topology_1.py
@@ -30,7 +30,7 @@ DATASET_PATH = "/scratch/kirschstein/LamaH-CE"
 CHECKPOINT_PATH = "./runs/topology"
 
 for fold, (train_years, test_years) in enumerate(functions.k_fold_cross_validation_split(range(2000, 2018), k=6)):
-    for architecture in ["GCN", "ResGCN", "GCNII"]:
+    for architecture in ["GCN"]:
         for edge_orientation in ["downstream", "upstream", "bidirectional"]:
             for adjacency_type in ["isolated", "binary", "stream_length", "elevation_difference", "average_slope", "learned"]:
                 hparams["training"]["train_years"] = train_years
