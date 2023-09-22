3de7e677d3374c3540b3ac3549a4550bc64050c4
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2023-09-22 11:42:37 +0200

    Update

--- evaluate_depth.py
+++ evaluate_depth.py
@@ -4,27 +4,27 @@ import pandas as pd
 
 from datetime import datetime
 
-
 DATASET_PATH = "/scratch/kirschstein/LamaH-CE"
-CHECKPOINT_PATH = "/scratch/kirschstein/runs/depth"
-OUT_FILE = f"results_{datetime.now()}.txt"
+CHECKPOINT_DIR = "./runs/depth"
+OUT_FILE = f"results_depth_{datetime.now()}.txt"
 
 results_string = ""
 for num_layers in range(1, 21):
     print(num_layers, "layers")
-    summary_df = pd.DataFrame()
+    summary_df = pd.DataFrame(columns=["mean_mse", "mean_nse"])
     fold_results = []
     for fold in range(6):
-        chkpt = functions.load_checkpoint(f"{CHECKPOINT_PATH}/layers{num_layers:02d}_{fold}.run")
+        chkpt = functions.load_checkpoint(f"{CHECKPOINT_DIR}/layers{num_layers:02d}_{fold}.run")
         model, dataset = functions.load_model_and_dataset(chkpt, DATASET_PATH)
         test_mse, test_nse = functions.evaluate_mse_nse(model, dataset)
         summary_df.loc[fold, "mean_mse"] = test_mse.mean().item()
         summary_df.loc[fold, "mean_nse"] = test_nse.mean().item()
         fold_results.append(torch.cat([test_mse, test_nse], dim=1))
+    fold_df = pd.DataFrame(torch.stack(fold_results).mean(dim=0), columns=["mean_mse", "mean_nse"])
     with open(OUT_FILE, "a") as f:
         f.write(f"{num_layers} layers\n")
-        f.write(str(pd.DataFrame(torch.stack(fold_results).mean(dim=0)).describe()) + "\n")
-        f.write(str(pd.DataFrame(summary_df).describe()) + "\n\n")
+        f.write(str(fold_df.describe()) + "\n")
+        f.write(str(summary_df.describe()) + "\n\n")
 
 with open(OUT_FILE, "r") as f:
     s = f.read()
