f3830308858c1a894f95486e11efd6988a0b55b5
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2023-09-22 10:05:20 +0200

    Update

--- functions.py
+++ functions.py
@@ -91,8 +91,7 @@ def load_dataset(path, hparams, split):
 
 
 def load_model_and_dataset(chkpt, dataset_path):
-    best_epoch = torch.tensor(chkpt["history"]["val_loss"]).argmin()
-    model_params = chkpt["history"]["model_params"][best_epoch]
+    model_params = chkpt["history"]["best_model_params"]
     dataset = load_dataset(dataset_path, chkpt["hparams"], split="test")
     model = construct_model(chkpt["hparams"], dataset)
     model.load_state_dict(model_params, strict=False)
