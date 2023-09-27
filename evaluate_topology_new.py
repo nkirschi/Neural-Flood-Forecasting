c43a5bd3910510a0e2359a4bfdf047639099c02c
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2023-09-27 20:16:24 +0200

    Update

--- evaluate_topology_new.py
+++ evaluate_topology_new.py
@@ -4,18 +4,19 @@ import pandas as pd
 from datetime import datetime
 
 DATASET_PATH = "/scratch/kirschstein/LamaH-CE"
-CHECKPOINT_DIR = "./runs/topology"
-OUT_FILE = f"results_topology_{datetime.now()}.csv"
+CHECKPOINT_DIR = "./runs/depth"
+OUT_FILE = f"results_depth_{datetime.now()}.csv"
 
 results_df = pd.DataFrame()
-for architecture in ["ResGCN", "GCNII"]:
-    for num_layers in range(1, 21):
-        for fold in range(6):
-            run_id = f"{architecture}_bidirectional_binary_depth{num_layers:02d}_{fold}"
-            print(run_id)
-            chkpt = functions.load_checkpoint(f"{CHECKPOINT_DIR}/{run_id}.run")
-            model, dataset = functions.load_model_and_dataset(chkpt, DATASET_PATH)
-            test_mse, test_nse = functions.evaluate_mse_nse(model, dataset)
-            results_df.loc[run_id + "_MSE", range(len(test_mse))] = test_mse.squeeze().numpy()
-            results_df.loc[run_id + "_NSE", range(len(test_mse))] = test_nse.squeeze().numpy()
+for architecture in ["GCN", "ResGCN", "GCNII"]:
+    for edge_orientation in ["downstream", "upstream", "bidirectional"]:
+        for adjacency_type in ["isolated", "binary", "stream_length", "elevation_difference", "average_slope", "learned"]:
+            for fold in range(6):
+                run_id = f"{architecture}_{edge_orientation}_{adjacency_type}_{fold}"
+                print(run_id)
+                chkpt = functions.load_checkpoint(f"{CHECKPOINT_DIR}/{run_id}.run")
+                model, dataset = functions.load_model_and_dataset(chkpt, DATASET_PATH)
+                test_mse, test_nse = functions.evaluate_mse_nse(model, dataset)
+                results_df.loc[run_id + "_MSE", range(len(test_mse))] = test_mse.squeeze().numpy()
+                results_df.loc[run_id + "_NSE", range(len(test_mse))] = test_nse.squeeze().numpy()
 results_df.to_csv(OUT_FILE)
