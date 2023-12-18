a26cf8926fb8204339d1b180e86c3b4ae18816e5
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2023-12-18 17:40:30 +0100

    Update

--- train_gadgets.py
+++ train_gadgets.py
@@ -2,7 +2,7 @@ import functions
 
 hparams = {
     "data": {
-        "base_gauge_id": None,  # set below,
+        "root_gauge_id": None,  # set below,
         "rewire_graph": False,
         "window_size": 24,
         "stride_length": 1,
@@ -24,7 +24,7 @@ hparams = {
         "weight_decay": 5e-4,
         "random_seed": 42,
         "train_years": None,  # set below
-        "holdout_size": 1/5,
+        "holdout_size": 1 / 5,
     }
 }
 
@@ -34,11 +34,11 @@ CHECKPOINT_PATH = "/scratch/kirschstein/runs/gadgets"
 for fold_id, (train_years, test_years) in enumerate([(list(range(2000, 2016, 2)), [2016, 2017]),
                                                      (list(range(2001, 2016, 2)), [2016, 2017]),
                                                      (list(range(2008, 2016, 1)), [2016, 2017])]):
-    for base_gauge_id in [71, 211, 387]:
+    for root_gauge_id in [71, 211, 387]:
         for architecture in ["ResGCN"]:  # ["GCN", "ResGCN", "GCNII"]
             for edge_orientation in ["bidirectional"]:  # ["downstream", "upstream", "bidirectional"]
                 for adjacency_type in ["isolated", "binary"]:  # ["isolated", "binary", "stream_length", "elevation_difference", "average_slope", "learned"]
-                    hparams["data"]["base_gauge_id"] = base_gauge_id
+                    hparams["data"]["root_gauge_id"] = root_gauge_id
                     hparams["training"]["train_years"] = train_years
                     hparams["model"]["architecture"] = architecture
                     hparams["model"]["edge_orientation"] = edge_orientation
@@ -53,6 +53,9 @@ for fold_id, (train_years, test_years) in enumerate([(list(range(2000, 2016, 2))
                     model = functions.construct_model(hparams, dataset)
                     history = functions.train(model, dataset, hparams)
 
-                    functions.save_checkpoint(history, hparams,
-                                              f"{architecture}_{edge_orientation}_{adjacency_type}_{fold_id}.run",
-                                              directory=CHECKPOINT_PATH)
+                    functions.save_checkpoint(
+                        history,
+                        hparams,
+                        f"{root_gauge_id}_{architecture}_{edge_orientation}_{adjacency_type}_{fold_id}.run",
+                        directory=CHECKPOINT_PATH
+                    )
