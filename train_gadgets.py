47622fda22b6af3970d457e62b0fceed6b2d4d16
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2023-12-20 15:59:25 +0100

    Update

--- train_gadgets.py
+++ train_gadgets.py
@@ -35,9 +35,9 @@ for fold_id, (train_years, test_years) in enumerate([(list(range(2000, 2016, 2))
                                                      (list(range(2001, 2016, 2)), [2016, 2017]),
                                                      (list(range(2008, 2016, 1)), [2016, 2017])]):
     for root_gauge_id in [71, 211, 387]:
-        for architecture in ["ResGCN"]:  # ["GCN", "ResGCN", "GCNII"]
-            for edge_orientation in ["bidirectional"]:  # ["downstream", "upstream", "bidirectional"]
-                for adjacency_type in ["isolated", "binary"]:  # ["isolated", "binary", "stream_length", "elevation_difference", "average_slope", "learned"]
+        for architecture in ["ResGCN", "GCNII"]:  # ["GCN", "ResGCN", "GCNII"]
+            for edge_orientation in ["downstream", "upstream", "bidirectional"]:
+                for adjacency_type in ["isolated", "binary", "stream_length", "elevation_difference", "average_slope", "learned"]:
                     hparams["data"]["root_gauge_id"] = root_gauge_id
                     hparams["training"]["train_years"] = train_years
                     dataset = functions.load_dataset(DATASET_PATH, hparams, split="train")
