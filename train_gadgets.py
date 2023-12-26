88921bb0d12de9547801d3a732fdb0314aad9312
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2023-12-26 20:40:15 +0100

    Update

--- train_gadgets.py
+++ train_gadgets.py
@@ -35,7 +35,7 @@ for fold_id, (train_years, test_years) in enumerate([(list(range(2000, 2016, 2))
                                                      (list(range(2001, 2016, 2)), [2016, 2017]),
                                                      (list(range(2008, 2016, 1)), [2016, 2017])]):
     for root_gauge_id in [71, 211, 387]:
-        for architecture in ["ResGCN", "GCNII"]:  # ["GCN", "ResGCN", "GCNII"]
+        for architecture in ["ResGCN", "GCNII"]:
             for edge_orientation in ["downstream", "upstream", "bidirectional"]:
                 for adjacency_type in ["isolated", "binary", "stream_length", "elevation_difference", "average_slope", "learned"]:
                     hparams["data"]["root_gauge_id"] = root_gauge_id
