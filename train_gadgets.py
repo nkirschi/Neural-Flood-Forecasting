b404f8bb9056911d53e3c1d2a60e0dd66c19dfd7
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2023-12-26 20:46:46 +0100

    Update

--- train_gadgets.py
+++ train_gadgets.py
@@ -34,7 +34,7 @@ CHECKPOINT_PATH = "/scratch/kirschstein/runs/gadgets"
 for fold_id, (train_years, test_years) in enumerate([(list(range(2000, 2016, 2)), [2016, 2017]),
                                                      (list(range(2001, 2016, 2)), [2016, 2017]),
                                                      (list(range(2008, 2016, 1)), [2016, 2017])]):
-    for root_gauge_id in [71, 211, 387]:
+    for root_gauge_id in [71, 211, 387, 532]:
         for architecture in ["ResGCN", "GCNII"]:
             for edge_orientation in ["downstream", "upstream", "bidirectional"]:
                 for adjacency_type in ["isolated", "binary", "stream_length", "elevation_difference", "average_slope", "learned"]:
