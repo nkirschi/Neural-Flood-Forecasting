0b33d691f762f40a0105f03d05d10bc7a7efe031
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2024-01-19 15:16:16 +0100

    Update

--- train_topology.py
+++ train_topology.py
@@ -20,8 +20,8 @@ hparams = {
     "training": {
         "num_epochs": 50,
         "batch_size": 64,
-        "learning_rate": 5e-4,
-        "weight_decay": 1e-4,
+        "learning_rate": 1e-3,
+        "weight_decay": 5e-4,
         "random_seed": 42,
         "train_years": None,  # set below
         "holdout_size": 1/5,
