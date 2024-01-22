539600b89e2c9d1935c5a7252bf512174755e90d
Nikolas Kirschstein <nikolas.kirschstein@gmail.com>
2024-01-22 17:07:48 +0100

    Update

--- evaluate_gadgets.py
+++ evaluate_gadgets.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 DATASET_PATH = "/scratch/kirschstein/LamaH-CE"
 CHECKPOINT_PATH = "/scratch/kirschstein/runs/gadgets"
-OUT_FILE = f"results/results_gadgets_{datetime.now()}.csv"
+OUT_FILE = f"{CHECKPOINT_PATH}/results.csv"
 
 results_df = pd.DataFrame()
 for root_gauge_id in [71, 211, 387, 532]:
@@ -17,7 +17,6 @@ for root_gauge_id in [71, 211, 387, 532]:
                     print(run_id)
                     chkpt = functions.load_checkpoint(f"{CHECKPOINT_PATH}/{run_id}.run")
                     model, dataset = functions.load_model_and_dataset(chkpt, DATASET_PATH)
-                    test_mse, test_nse = functions.evaluate_mse_nse(model, dataset)
-                    results_df.loc[run_id + "_MSE", range(len(test_mse))] = test_mse.squeeze().numpy()
-                    results_df.loc[run_id + "_NSE", range(len(test_mse))] = test_nse.squeeze().numpy()
+                    test_nse = functions.evaluate_mse_nse(model, dataset)
+                    results_df.loc[run_id, range(len(test_nse))] = test_nse.squeeze().numpy()
 results_df.to_csv(OUT_FILE)
--- evaluate_topology.py
+++ evaluate_topology.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 DATASET_PATH = "/scratch/kirschstein/LamaH-CE"
 CHECKPOINT_PATH = "/scratch/kirschstein/runs/topology"
-OUT_FILE = f"results/results_topology_{datetime.now()}.csv"
+OUT_FILE = f"{CHECKPOINT_PATH}/results.csv"
 
 results_df = pd.DataFrame()
 for architecture in ["ResGCN", "GCNII"]:
@@ -16,7 +16,6 @@ for architecture in ["ResGCN", "GCNII"]:
                 print(run_id)
                 chkpt = functions.load_checkpoint(f"{CHECKPOINT_PATH}/{run_id}.run")
                 model, dataset = functions.load_model_and_dataset(chkpt, DATASET_PATH)
-                test_mse, test_nse = functions.evaluate_mse_nse(model, dataset)
-                results_df.loc[run_id + "_MSE", range(len(test_mse))] = test_mse.squeeze().numpy()
-                results_df.loc[run_id + "_NSE", range(len(test_mse))] = test_nse.squeeze().numpy()
+                test_nse = functions.evaluate_mse_nse(model, dataset)
+                results_df.loc[run_id, range(len(test_nse))] = test_nse.squeeze().numpy()
 results_df.to_csv(OUT_FILE)
